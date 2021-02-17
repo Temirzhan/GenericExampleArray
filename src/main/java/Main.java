c83b86bc19e2f65d525e7a5582b1e5003764c678
Azamat <azamattemirzhan@gmail,com>
2021-02-17 21:41:47 +0300

    asdasd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        GenericsExample A =new GenericsExample();
+
+        String[] a = {"a","b","s"};
+        String[] b = {"s","a","b"};
+        String[] c = new String[5];
+        A.swapArrat(a,b);
+
+        for (String s:a) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        for (String s:b) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+
+        ArrayList<String> S = new ArrayList<String>(A.arrayToArrayList(a));
+
+        System.out.println(S);
+    }
 }
